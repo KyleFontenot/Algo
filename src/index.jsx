3270bd3790fec7f67ae17394124a4b508723cce2
KyleFontenot <kylewilliamfontenot@gmail.com>
2022-01-11 10:30:50 -0800

    Initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -4,35 +4,16 @@ import { render } from "solid-js/web";
 // import type { Component } from "solid-js";
 import { lazy, onMount } from "solid-js";
 import { Router, Routes, Route } from "solid-app-router";
-import state from "./components/StateProvider";
 
-// import logo from "./logo.svg";
 import styles from "./App.module.css";
 import "./styles/globals.css";
 import "./styles/custom.scss";
 
 import "./index.css";
-// import App from "./App";
 
-const Compare = lazy(() => import("./pages/compare.jsx"));
 const Home = lazy(() => import("./pages/index.jsx"));
 
 const App = () => {
-  const {
-    stock1,
-    stock2,
-    symbInit1,
-    calibrate1,
-    calibrate2,
-    correlationFactor,
-  } = state;
-
-  onMount(async () => {
-    if (!stock1() && localStorage.getItem("storedstock1")) {
-      calibrate1(JSON.parse(localStorage.getItem("storedstock1")));
-    }
-  });
-
   return (
     <Router>
       <Routes>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,60 +1,10 @@
-import SearchBox from "../components/SearchBox/SearchBox";
-import MainLogo from "..//components/MainLogo/MainLogo";
-import StockCard from "../components/StockCard/StockCard";
 import styles from "../styles/Home.module.scss";
 import { onMount } from "solid-js";
-import state from "../components/StateProvider";
-import { useNavigate } from "solid-app-router";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const {
-    stock1,
-    stock2,
-    symbInit1,
-    calibrate1,
-    calibrate2,
-    correlationFactor,
-  } = state;
-
-  onMount(async () => {
-    if (!stock1() && localStorage.getItem("storedstock1")) {
-      await calibrate1(JSON.parse(localStorage.getItem("storedstock1")));
-      // navigate("/compare", { replace: false });
-    }
-  });
-
   return (
     <>
-      <div className={styles.gridwrapper}>
-        <MainLogo className={styles.mainlogo} />
-        <SearchBox description className={styles.searchBox} />
-        <Show when={stock1()}>
-          <StockCard
-            outlined
-            symbol={stock1()?.Item?.ticker}
-            name={stock1()?.Item?.name}
-            style="margin-top: 1rem; max-width: 20rem;"
-            className={styles.stockcard}
-          />
-        </Show>
-      </div>
-      <div className={styles.attribution}>
-        <p>
-          Made by: &nbsp;
-          <a
-            href="https://ericoulster.github.io/Personal-Site/"
-            target="__blank"
-            rel="noopener"
-          >
-            Eric Oulster
-          </a>
-          , &nbsp;
-          <a href="https://kylefontenot.com" target="__blank" rel="noopener">
-            Kyle Fontenot
-          </a>
-        </p>
-      </div>
+      <p>hello</p>
     </>
   );
 };
